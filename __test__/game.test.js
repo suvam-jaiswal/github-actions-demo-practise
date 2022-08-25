f267639227711ae0425a6368b4022f8a1af3dd15
suvam-jaiswal <suvam.jaiswal@oneadvanced.com>
2022-08-25 21:36:05 +0530

    fix: trigger workflow

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,7 +1,7 @@
 const Game = require('../src/game').default
 const fs = require('fs')
 
-describe('Appjs', () => {
+describe('App', () => {
   it('Contains the compiled JavaScript', async (done) => {
     fs.readFile('./public/main.js', 'utf8', (err, data) => {
       expect(err).toBe(null)
